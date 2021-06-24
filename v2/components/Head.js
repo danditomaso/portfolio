730fe24a033617fa0118907e7b9cdbe3e77eb904
Dan Ditomaso <danditomaso@users.noreply.github.com>
2021-06-24 07:41:06 -0400

    adding meta description, alt text for images (#1)

--- v2/components/Head.js
+++ v2/components/Head.js
@@ -11,7 +11,12 @@ const Social = () => {
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Playfair+Display:wght@700&display=swap"
         rel="stylesheet"
       />
+      <meta http-equiv="v-ua-compatible" content="ie=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="A friendly Toronto based frontend developer. I focus on React, Redux, NodeJS, NextJS, and Vanilla JS"
+      />
       <meta charSet="utf-8" />
       <title>Dan Ditomaso</title>
       <meta property="og:title" content={siteName} key="ogtitle" />
